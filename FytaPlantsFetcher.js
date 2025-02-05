42922bf153b56c3db99f10da8274d9cc86e37b3f
filetto <philipp.bretscher@bluewin.ch>
2025-02-05 12:28:41 +0100

    Update FytaPlantsFetcher.js

--- FytaPlantsFetcher.js
+++ FytaPlantsFetcher.js
@@ -58,7 +58,7 @@ const FytaPlantsFetcher = function (config) {
 
     // 📡 Übergib `plants` & `lastUpdate` an den Node Helper
     const dataToSend = {
-        plants: plants,
+        plants: plants.plants,
         lastUpdate: lastUpdate
     };
 
